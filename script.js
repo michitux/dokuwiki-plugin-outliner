7173ea627c4b936d36d13be7c0d2c90c9f2eb76f
Michael Hamann <michael@content-space.de>
2010-03-12 19:28:19 +0100

    Almost complete rewrite.
    There are probably still a lot of bugs around, but it works again
    including preview, saved states, ...
    The code has been almost completely rewritten, it now uses a lot more of
    CSS (e.g. for previews), the JavaScript is unobtrusive, and proper
    nesting of syntax modes is ensured by using start/end tags. The HTML has
    been changed completely, too.

--- script.js
+++ script.js
@@ -1,177 +1,54 @@
 /**
  * Outliner plugin JS library
  *
- * @author Pavel Vitis <pavel [dot] vitis [at] seznam [dot] cz>
+ * @author Michael Hamann <michael [at] content-space [dot] de>
  */
 
-function setStableCookie(name, value) {
-    var expire = new Date();
-    fixDate(expire);
-    expire.setTime(expire.getTime()+365*24*60*60*1000);
-    setCookie(name, value, expire);
-}
+addInitEvent(function () {
+  var outliner_dls = getElementsByClass('outliner', document, 'dl');
 
-function outliner_writeLayer(layerID, txt) {
-    try {
-	if (document.getElementById) {
-    	    var layer = document.getElementById(layerID);
-            if (layer) {
-	      layer.innerHTML = txt;
-	    }
-        }
-	else {
-            if (document.all) {
-		document.all[layerID].innerHTML = txt;
-	    }
-	    else {
-		if (document.layers) {
-                    var layer_content = document.layers[layerID].document;
-                    layer_content.open();
-                    layer_content.open();
-                    layer_content.write(txt);
-                    layer_content.close();
-		}
-	    }
-	}
-   }
-   catch (e) {}
-}
-
-function outliner_isOpen(div) {
-  if (typeof(div) == 'string') {
-    div = document.getElementById('outliner_div_'+div);
-  }
-  try {
-    return (div.className.indexOf(' visible') != -1);
-  } catch (e) {
-    div.visible = 0;
-    return false;
-  }
-}
-
-function outliner_openByDefault(div) {
-  if (typeof(div) == 'string') {
-    div = document.getElementById('outliner_div_'+div);
-  }
-  try {
-    return (div.className.indexOf('default-open') != -1);
-  } catch (e) {
-    return false;
-  }
-}
-
-function outliner_switch(out, show) {
-  if (!document.getElementById) {
-    return NULL;
-  }
-
-  var div = document.getElementById("outliner_div_"+out);
-  var link = document.getElementById("outliner_wedge_"+out);
-  if (typeof(show) == 'undefined') {
-    show = !outliner_isOpen(out);
-  }
-  else {
-    if ((show === false) && !outliner_isOpen(out)) {
-      return false;
+  var setState = function(node, state) {
+    if (state != 'open' && state != 'closed') return;
+    var otherState = (state == 'open') ? 'closed' : 'open';
+    if (node.className.indexOf('outliner-' + state) ==  -1 && node.className.indexOf('outliner-' + otherState) == -1) {
+      node.className += ' outliner-' + state;
+    } else {
+      if (state == 'open') {
+        node.className = node.className.replace(/outliner-closed/, 'outliner-' + state);
+      } else {
+        node.className = node.className.replace(/outliner-open/, 'outliner-' + state);
+      }
+    }
+    var nodeId = getOutlinerId(node);
+    if (nodeId.length) {
+      DokuCookie.setValue(nodeId, state);
     }
   }
 
-  if (show) {
-//    alert(div.style.border);
-    link.className = link.className.replace(/ wedgeClosed/gi, ' wedgeOpen');
-    div.className = div.className.replace(/ invisible/gi, ' visible');
-//	  div.style.height = 1;
-    div.visible = 1;
-    setStableCookie('outlineState_'+out, 'opened');
-    return true;
+  var getOutlinerId = function(node) {
+    return node.className.match(/outliner_\w+/)[0];
   }
-  else {
-    link.className = link.className.replace(/ wedgeOpen/gi, ' wedgeClosed');
-    div.className = div.className.replace(/ visible/gi, ' invisible');
-    div.height = 0;
-    div.visible = 0;
-    setStableCookie('outlineState_'+out, 'collapsed');
-    return false;
-  }
-}
 
-function outliner_toggleLine(out, show) {
-  if (!document.getElementById) {
-    return;
-  }
-  var div = document.getElementById("outliner_div_"+out);
-  if (outliner_isOpen(out)) {
-    var bar = document.getElementById("outliner_bar_"+out);
-    if (show) {
-//    alert(div.style.border);
-	div.className = div.className.replace(/ unmarked/gi, ' marked');
-	bar.className = bar.className.replace(/ unmarked/gi, ' marked');
-    }
-    else {
-	div.className = div.className.replace(/ marked/gi, ' unmarked');
-	bar.className = bar.className.replace(/ marked/gi, ' unmarked');
-    }
-  }
-}
+  for (var i = 0; i < outliner_dls.length; i++) {
+    outliner_dls[i].className += ' outliner-js';
 
-function outliner_loadState(out) {
-  var state = getCookie('outlineState_'+out);
-//  alert(state);
-//  alert(out);
-  var show;
-  if ((null == state) && outliner_openByDefault(out)) {
-    show = true;
-  }
-  else {
-    show = (state == 'opened');
-  }
-  outliner_switch(out, show);
-}
-
-function outliner_setPopup(out, e, evt) {
-  var div = document.getElementById("outliner_div_"+out);
-  if (!outliner_isOpen(out)) {
-    var content = new String(document.getElementById('outliner_content_'+out).innerHTML);
-    content = content.replace(/<div class=\"secedit\">.*?<\/div>/gi, '');
-    var popup = document.getElementById("outliner_popup_"+out);
-    if (document.getElementById("outliner_popup_"+out)) {
-	outliner_writeLayer('outliner_popup_'+out, content);
+    setState(outliner_dls[i], DokuCookie.getValue(getOutlinerId(outliner_dls[i])));
+    if (outliner_dls[i].className.indexOf('outliner-open') ==  -1 && outliner_dls[i].className.indexOf('outliner-closed') == -1) {
+      setState(outliner_dls[i], 'closed');
     }
 
-    domTT_activate(
-      e,
-      evt,
-      'content', content,
-      'type', 'velcro',
-      'fade', 'out',
-      'offsetX', 0, 'offsetY', 5,
-//    'clearMouse', true,
-//    'trail', true,
-      'lazy', false,
-      'id', 'outliner_popup_'+out,
-      'styleClass', 'floating',
-      'maxWidth', (document.body.offsetWidth*0.8));
-
-//    popup.style.display = 'block';
-//    popup.style.left = document.mouseX + 0;
-//    popup.style.top = document.mouseY + 10;
-//    divopup.className = 'outline floating';
+    addEvent(outliner_dls[i].getElementsByTagName('dt')[0], 'click', function() {
+      if (this.parentNode.className.indexOf('outliner-open') == -1) {
+        setState(this.parentNode, 'open');
+      } else {
+        setState(this.parentNode, 'closed');
+      }
+    });
+    addEvent(outliner_dls[i].getElementsByTagName('dt')[0], 'mouseover', function() {
+      with (this.parentNode.getElementsByTagName('dd')[0]) {
+        style.left = findPosX(this)+40 + 'px';
+        style.top = findPosY(this)+20 + 'px';
+      }
+    });
   }
-  clearTimeout();
-}
-
-var outliner_tmid = null;
-
-function outliner_showPopup(out, e, evt) {
-  outliner_setPopup(out, e, evt);
-}
-
-function outliner_clearPopup(out) {
-  if (null !== outliner_tmid) {
-    clearTimeout(outliner_tmid);
-    outliner_tmid = null;
-  }
-  var popup = document.getElementById('outliner_popup_'+out);
-  outliner_writeLayer('outliner_popup_'+out, '');
-  domTT_close('outliner_popup_'+out);
-}
+});
---
+++ syntax.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Outliner Plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Michael Hamann <michael [at] content-space [dot] de>, Pavel Vitis <pavel [dot] vitis [at] seznam [dot] cz>
+ */
+
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_outliner extends DokuWiki_Syntax_Plugin {
+
+    /**
+     * return some info
+     */
+    function getInfo(){
+        return array(
+            'author' => 'Michael Hamann',
+            'email'  => 'michael@content-space.de',
+            'date'   => '2010-03-10',
+            'name'   => 'Outliner Plugin',
+            'desc'   => 'Allows collapsible outline layout',
+            'url'    => 'http://dokuwiki.org/plugin:outliner',
+        );
+    }
+
+    /**
+     * What kind of syntax are we?
+     */
+    function getType(){
+        return 'container';
+    }
+
+    function getAllowedTypes() {
+        return array('container', 'baseonly', 'formatting', 'substition', 'protected', 'disabled', 'paragraphs');
+    }
+
+    /* Accept own mode so nesting is allowed */
+    function accepts($mode) {
+      if ($mode == substr(get_class($this), 7)) return true;
+      return parent::accepts($mode);
+    }
+
+    function getPType(){
+        return 'block';
+    }
+
+    /**
+     * Where to sort in?
+     */
+    function getSort(){
+        return 100;
+    }
+
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+        $this->Lexer->addEntryPattern(
+            '\n\s*-->[^\n]*(?=.*?\n\s*<--[^\n]*)',
+            $mode,
+            'plugin_outliner');
+    }
+
+    function postConnect() {
+        $this->Lexer->addExitPattern(
+            '\n\s*<--[^\n]*',
+            'plugin_outliner');
+    }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler){
+        global $ID;
+
+        switch ($state) {
+        case DOKU_LEXER_ENTER:
+            $matches = array();
+            preg_match('/-->\s*([^#^]*)([#^]*)/', $match, $matches);
+            $title = $matches[1];
+            $outline_id = ''.md5($ID).'_'.mt_rand(100000,999999);
+
+            // Test if '^ - opened node' flag is present
+            $opened = (strpos($matches[2], '^') !== false);
+            // Test if '# - no popup' flag is present
+            $nopopup = (strpos($matches[2], '#') !== false);
+            return array($state, $title, $outline_id, $opened, $nopopup);
+
+        case DOKU_LEXER_EXIT:
+            return array($state);
+
+        case DOKU_LEXER_UNMATCHED :
+            return array($state, $match);
+
+        default:
+            return array();
+        }
+    }
+
+    /**
+     * Create output
+     */
+    function render($mode, &$renderer, $data) {
+      if ($mode == 'xhtml') {
+          $state = $data[0];
+          switch ($state) {
+          case DOKU_LEXER_ENTER:
+              list($state, $title, $outline_id, $opened, $nopopup) = $data;
+              $renderer->doc .= '<dl class="outliner outliner_'.$outline_id;
+              if ($opened) $renderer->doc .= ' outliner-open';
+              if ($nopopup) $renderer->doc .= ' outliner-nopopup';
+              $renderer->doc .= '"><dt>'.hsc($title)."</dt><dd>\n";
+              break;
+          case DOKU_LEXER_EXIT:
+              $renderer->doc .= "</dd></dl>\n";
+              break;
+          case DOKU_LEXER_UNMATCHED:
+              $renderer->doc .= $renderer->_xmlEntities($data[1]);
+              break;
+          }
+          return true;
+      }
+      return false;
+    }
+}
+
+//Setup VIM: ex: et ts=4 enc=utf-8 :
